50ad0f49376f9e04cff1272b3b108c7339463585
kouki <kouworld0123@gmail.com>
2017-05-26 12:55:19 +0900

    make package and make Class
    make img and highPathLowPath Packages
    make ImageUtility.java in img package

---
+++ src/highPathLowPath/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author kouki
+ *
+ */
+package highPathLowPath;
---
+++ src/img/ImageUtility.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package img;
+
+/**
+ * @author kouki
+ *
+ */
+public class ImageUtility {
+
+}
