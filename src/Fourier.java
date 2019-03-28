cd389111555b74ff16c2ef88ee5f74e0908cdf4e
kouki <kouworld0123@gmail.com>
2019-03-29 01:50:36 +0900

    :+1: フーリエ変換の型枠

--- src/Fourier.java
+++ src/Fourier.java
@@ -1,4 +1,25 @@
-
 public class Fourier {
-
+	public static void main(String arg[])
+	{
+		//画像読み込み
+		//サイズ取得
+		//double型配列
+		
+	}
+	
+	public static void FFT2D(
+			double[][] inDataRe, double[][] inDataIm, 
+			double[][] outDataRe, double[][] outDataIm, 
+			int xSize, int ySize)
+	{
+		//2次元フーリエ変換
+	}
+	
+	public static void IFFT2D(
+			double[][] inDataRe, double[][] inDataIm, 
+			double[][] outDataRe, double[][] outDataIm, 
+			int xSize, int ySize)
+	{
+		//2次元フーリエ変換
+	}
 }
